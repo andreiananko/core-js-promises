3a503cef23a5d61ce1fa3d17880e3bb435391e69
andreiananko <110332587+andreiananko@users.noreply.github.com>
2024-04-16 02:13:11 +0300

    init: start the task feat: added function getPromise(number)

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,9 +17,16 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  return new Promise((resolve, reject) => {
+    if (number >= 0) {
+      resolve(number); // Fulfill the promise with the input number
+    } else {
+      reject(new Error('Number is negative')); // Reject the promise with an error
+    }
+  });
 }
+
 
 /**
  * Returns a promise that will always fulfilled and return a value of success or fail.
