be075ae9b5b343234f01153bac1356ba4407e0ac
andreiananko <110332587+andreiananko@users.noreply.github.com>
2024-04-16 02:18:20 +0300

    feat: added function getFirstResolvedPromiseResult(promises)

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -64,8 +64,24 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    let resolved = false;
+    promises.forEach((promise) => {
+      promise
+        .then((value) => {
+          if (!resolved) {
+            resolved = true;
+            resolve(value);
+          }
+        })
+        .catch(() => {
+          if (promises.every((p) => p.status === 'rejected')) {
+            reject(new Error('All promises are rejected'));
+          }
+        });
+    });
+  });
 }
 
 /**
