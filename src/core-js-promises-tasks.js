8e2304025e25e256c498ff525c009ae91b42f95e
andreiananko <110332587+andreiananko@users.noreply.github.com>
2024-04-16 02:28:25 +0300

    fix: fixed name function queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -183,7 +183,7 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(promises) {
+function queuPromises(promises) {
   let result = '';
   let chain = Promise.resolve();
 
